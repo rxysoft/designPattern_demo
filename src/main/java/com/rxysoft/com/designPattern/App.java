4c7cb26f1f5372605c7d0958f77b4d43aa509932
Administrator <Administrator@PC201810081529>
2018-12-12 09:17:16 +0800

    提交到本地

---
+++ src/main/java/com/rxysoft/com/designPattern/App.java
@@ -0,0 +1,13 @@
+package com.rxysoft.com.designPattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
