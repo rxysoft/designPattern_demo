b0b3fc16c9525eb5dc46ddaba3943272c650b8eb
Administrator <Administrator@PC201810081529>
2018-12-13 09:01:55 +0800

    提交备注

---
+++ src/main/java/com/rxysoft/adapterPattern/documentIntroduction.java
@@ -0,0 +1,41 @@
+package com.rxysoft.adapterPattern;
+
+public class documentIntroduction {
+    /*1适配器模式使用场景*********************************
+                   一 系统需要使用现有的类，但现有的类却不兼容。
+                   二 需要建立一个可以重复使用的类，用于协调一些彼此关系不大的类，并易于扩展，以便于面对将来会出现的类。
+                   三 需要一个统一的输出接口，但是输入类型却不可预知。
+     ***********************************************
+     *2适配器模式在开源项目中的应用
+      java.util.Arrays#asList()
+	  javax.swing.JTable(TableModel)
+	  java.io.InputStreamReader(InputStream)
+      java.io.OutputStreamWriter(OutputStream)
+      javax.xml.bind.annotation.adapters.XmlAdapter#marshal()
+      javax.xml.bind.annotation.adapters.XmlAdapter#unmarshal()
+      android开发中，ListView、RecyclerView等列表展示组件填充数据都会用到Adapter，这里面的Source相当于Datas，Destination相当于Views，然后Adapter去协调数据与显示。
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
+}
