de2fa0fd37d1a1b18ec397aa1e2fe709aa90532e
lxm <459734103@qq.com>
2019-05-22 20:11:54 +0800

    544

--- src/main/java/com/rxysoft/abstractFactoryPattern/App.java
+++ src/main/java/com/rxysoft/abstractFactoryPattern/App.java
@@ -3,6 +3,6 @@ package com.rxysoft.abstractFactoryPattern;
 public class App {
     public static void main( String[] args )
     {
-        System.out.println( "Hello333 World!" );
+        System.out.println( "Hello4443 World!" );
     }
 }
